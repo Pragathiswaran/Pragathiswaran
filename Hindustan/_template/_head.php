b184a215a7116e5ac5cb4ac11132272ff8a06fca
Pragathiswaran <mpragathiswaran@gmail.com>
2024-01-08 23:05:47 +0530

    new commit

--- Hindustan/_template/_head.php
+++ Hindustan/_template/_head.php
@@ -4,6 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hindustan Institute of Technology & Science - Student/Parent Portal</title>
     <link rel="icon" href="image/logo.png" type="image/x-icon">
-    <!-- <script src="js/authentication.js" defer></script> -->
+    <script src="js/authentication.js" defer></script>
     <link rel="stylesheet" href="/timeline/Hindustan/css/<?php print(basename($_SERVER['PHP_SELF'],'.php')) ?>.css">
 </head>
--- Hindustan/libs/load.php
+++ Hindustan/libs/load.php
@@ -1,19 +1,12 @@
 <?php
 include 'class/class.connection.php';
 include 'class/class.validation.php';
+include 'class/class.session.php';
 
 function load_template($name){
     
     include $_SERVER['DOCUMENT_ROOT']."/timeline/Hindustan/_template/$name.php";
 }
 
-function authentication($username, $password) {
-    if ($username === "pragathis" && $password === "praga@0009") {
-        return true;
-    } else {
-        // $boolean_value = array("username"=>false, "password"=>false);
-        return array("username"=>false, "password"=>false);
-    }
-}
 
 ?>
