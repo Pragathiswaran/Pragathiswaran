3ecf1b831ad773d41dfdd5a2ee7a62d1a82c7884
Pragathiswaran <mpragathiswaran@gmail.com>
2024-01-14 15:25:03 +0530

    commits

--- Hindustan/_template/_index.php
+++ Hindustan/_template/_index.php
@@ -1,24 +1,39 @@
 <div class="dropdown">
-    <il class="btn-circle">
-       <a href="">hello</a> 
-    </il>
-    <il class="btn-circle">
-        <a href="">hello</a>
-    </il>
-    <il class="btn-circle">
-        <a href="">hello</a>
-    </il>
-    <il class="btn-circle">
-        <a href="">hello</a>
-    </il>
-    <il class="btn-circle">
-        <a href="">hello</a>
-    </il>
-    <il class="btn-circle">
-        <a href="">hello</a>
-    </il>
+    <div class="dropdown-icon">
+        <svg>
+            <circle cx="37" cy="37" r="30" fill="blue" />
+        </svg>
+        <p>Home</p>
+    </div>
+    <div class="dropdown-icon">
+        <svg>
+            <circle cx="37" cy="37" r="30" fill="lightgreen" />
+        </svg>
+        <p>My Account</p>
+    </div>
+    <div class="dropdown-icon">
+        <svg>
+            <circle cx="37" cy="37" r="30" fill="violet" />
+        </svg>
+        <p>Academics</p>
+    </div>
+    <div class="dropdown-icon">
+        <svg>
+            <circle cx="37" cy="37" r="30" fill="yellow" />
+        </svg>
+        <p>Library</p>
+    </div>
+    <div class="dropdown-icon">
+        <svg>
+            <circle cx="37" cy="37" r="30" fill="blue" />
+        </svg>
+        <p>Hostel</p>
+    </div>
+    <div class="dropdown-icon">
+        <svg>
+            <circle cx="37" cy="37" r="30" fill="lightgreen" />
+        </svg>
+        <p>Payments</p>
+    </div>
 </div>
 
-<svg height="75" width="75">
-  <circle cx="37" cy="37" r="30" fill="red" />
-</svg>
